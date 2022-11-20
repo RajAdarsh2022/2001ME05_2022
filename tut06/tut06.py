55ba2df518956738e5a8e9ffb2309e2cce89be77
Adarsh <inforaj.2022@gmail.com>
2022-11-20 18:09:55 +0530

    Final Commits

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -12,16 +12,16 @@ import openpyxl
 start_time = datetime.now()
 
 def attendance_report():
-#Removing try except blocks for commit   
+    try:
         inp_file = pd.read_csv('input_attendance.csv')
         inp = inp_file.fillna("2001CCXX Random")
-    
+    except:
         print("File not found")
     
     
-    
+    try:
         rollno_inp=pd.read_csv('input_registered_students.csv')
-    
+    except:
         print('File containing name of all students is missing!')
     
 
