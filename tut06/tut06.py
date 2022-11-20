a6d195f48db66776224706e7f921c1f6ebbf7388
Adarsh <inforaj.2022@gmail.com>
2022-11-20 18:07:06 +0530

    Commit4

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -12,16 +12,16 @@ import openpyxl
 start_time = datetime.now()
 
 def attendance_report():
-    try:
+#Removing try except blocks for commit   
         inp_file = pd.read_csv('input_attendance.csv')
         inp = inp_file.fillna("2001CCXX Random")
-    except:
+    
         print("File not found")
     
     
-    try:
+    
         rollno_inp=pd.read_csv('input_registered_students.csv')
-    except:
+    
         print('File containing name of all students is missing!')
     
 
@@ -30,7 +30,15 @@ def attendance_report():
     mc=sum(1 for row in open("input_registered_students.csv"))
     mc_consolidated=sum(1 for row in open("input_attendance.csv"))
     total_dates=list()
-
+    for j in range(0,mc_consolidated-1):
+        day=inp.at[j,'Timestamp'].split()[0].split('-')[0]
+        month=inp.at[j,'Timestamp'].split()[0].split('-')[1]
+        year=inp.at[j,'Timestamp'].split()[0].split('-')[2]
+        date=datetime.strptime(f'{year}-{month}-{day}', "%Y-%m-%d").date()
+        day_name=date.strftime("%A")
+        if day_name=="Monday" or day_name=="Thursday":
+            if inp.at[j,'Timestamp'].split()[0] not in total_dates:
+                total_dates.append(inp.at[j,'Timestamp'].split()[0])
     # max_att=0
     # max_roll=""
     fileName_consolidated=".\output\\attendance_report_consolidated.xlsx"
